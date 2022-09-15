b3b525e9730e9b97feb4310d6e3d788f34181cad
Daniel Melo <daniel.melo42@outlook.com>
2022-09-14 18:04:08 -0600

    routes added

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 
-app.use('/', require('./routes'))
+app.use('/', require('./routes'));
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
